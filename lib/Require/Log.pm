ddb5f1246da7ebb5a67f1aa80500fca29d71abae
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-11-15 16:39:04 +0700

    WIP

--- lib/Require/Log.pm
+++ lib/Require/Log.pm
@@ -11,13 +11,25 @@ use warnings;
 # DIST
 # VERSION
 
-# saving the original require() does snot work
-#my $orig_require = \&CORE::GLOBAL::require;
-*CORE::GLOBAL::require = sub {
-    my @caller = caller(1);
-    warn "require($_[0]) called from package $caller[0] file $caller[1]:$caller[2]\n";
-    CORE::require(@_);
-};
+# check if building archive
+sub _is_dzil_build { my $i = 1; while (my @c = caller($i++)) { return 1 if $c[3] eq 'Dist::Zilla::Dist::Builder::build_archive' } 0 }
+
+my $patched;
+
+{
+    last if $patched;
+    last if _is_dzil_build(); # avoid patching if we are loaded during build
+
+    # saving the original require() does snot work
+    #$orig_require = \&CORE::GLOBAL::require;
+    *CORE::GLOBAL::require = sub {
+        my @caller = caller(1);
+        warn "require($_[0]) called from package $caller[0] file $caller[1]:$caller[2]\n";
+        warn "\@_: ".join(", ", @_)."\n";
+        CORE::require(@_);
+    };
+    $patched++;
+}
 
 1;
 # ABSTRACT: Log require()
@@ -29,6 +41,9 @@ use warnings;
 
 
 =head1 DESCRIPTION
+
+XXX. Currently does not work properly because we add a level of call in
+require().
 
 
 =head1 SEE ALSO
